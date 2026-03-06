0db60bd8dd079d60e4aecbd5a8caf8ae5ddacf0c
Ahmed Ennouri <2024705436@lacatholille.fr>
2026-03-06 11:06:41 +0100

    Etape 3 : création du repository

---
+++ src/main/java/com/studyplanner/studyplanner/repository/StudySessionRepository.java
@@ -0,0 +1,14 @@
+package com.studyplanner.studyplanner.repository;
+
+import com.studyplanner.studyplanner.entity.StudySession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudySessionRepository extends JpaRepository<StudySession, Long> {
+
+    List<StudySession> findByStudentName(String studentName);
+
+    long countByStudentName(String studentName);
+
+}
